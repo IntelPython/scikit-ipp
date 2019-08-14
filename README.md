f9a7c8c214c698a716d0f5e803c4f6ee9c38eadb
Nasibli, Samir <samir.nasibli@intel.com>
2019-08-14 08:58:36 -0500

    change setup and readme files

--- README.md
+++ README.md
@@ -3,15 +3,3 @@
 * filters (Gaussian)
 
 ### TODO
-1) fix bug for RGB images [case: output is None]
-2) handle the ipp status check
-3) delete numChannels from funcs signature
-4) currently `gaussian` doesn't use multichannel, cval, truncate values (will fix)
-5) integrate numpy conversions
-6) test on scikit-image gausssian_test
-7) multichannels
-8) code style
-
-### issues:
-1) Ipp has more than 6 types of borders, is it needed to use all of them?
-2) conversions in-place or not?
--- filters/setup.py
+++ filters/setup.py
@@ -3,19 +3,14 @@ from Cython.Build import cythonize
 from numpy import get_include
 
 if __name__ == '__main__':
-    module1 = Extension("Scikit_IPP_filters",
-                        ["GaussianFilter_wrapper.pyx"],
+    gaussian_module = Extension("Scikit_IPP_filters", 
+                        ["_edges.pyx"], 
                         language="c",
-                        include_dirs=[get_include(), "$IPPROOT/include"],
-                        libraries=["ippcv",
-                                   "ippcore",
-                                   "ippvm",
-                                   "ipps",
-                                   "ippi"],
-                        library_dirs=["$IPPROOT/intel64_win"]
+                        include_dirs=[get_include(),"$IPPROOT/include"],
+                        libraries = ["ippcv","ippcore", "ippvm", "ipps", "ippi"],
+                        library_dirs = ["$IPPROOT/lib/intel64_lin"]
                         )
-
-    setup(name='Scikit_IPP_filters',
-          version='1.0',
-          ext_modules=cythonize([module1])
-          )
+    setup (name = 'Scikit_IPP_filters',
+           version = '1.0',
+           ext_modules = cythonize([gaussian_module])
+           )
