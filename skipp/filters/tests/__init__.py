152cb6ec321da5a328a96c78280659832556c9e7
Nasibli, Samir <samir.nasibli@intel.com>
2019-09-19 09:22:51 -0500

    aligned codestyle for the tests with PEP8

--- skipp/filters/tests/__init__.py
+++ skipp/filters/tests/__init__.py
@@ -1,5 +1,6 @@
 def setup_module(module):
     pass
 
+
 def teardown_module(module):
     pass
--- skipp/filters/tests/test_gaussian.py
+++ skipp/filters/tests/test_gaussian.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 from skipp.skipp.filters import gaussian
 
+
 @pytest.fixture
 def image():
     return np.array([[1, 2, 3, 2, 1],
@@ -10,6 +11,7 @@ def image():
                      [3, 2, 1, 1, 1],
                      [1, 2, 1, 2, 3]],
                     dtype=np.uint8)
+
 
 @pytest.mark.parametrize(
     "output_dtype", [np.uint8, np.uint16, np.int16, np.float32]
@@ -21,15 +23,18 @@ def test_gaussian_output_dtype(image, input_dtype, output_dtype):
     gaussian_image = gaussian(image.astype(input_dtype), output=output_dtype)
     assert gaussian_image.dtype == output_dtype
 
+
 def test_null_sigma():
     a = np.zeros((3, 3), dtype=np.uint8)
     a[1, 1] = 1
     assert np.all(gaussian(a, output=np.uint8, sigma=0) == a)
 
+
 def test_default_sigma():
     a = np.zeros((3, 3))
     a[1, 1] = 1.
     assert np.all(gaussian(a) == gaussian(a, sigma=1))
+
 
 def test_energy_decrease():
     a = np.zeros((3, 3))
@@ -37,20 +42,24 @@ def test_energy_decrease():
     gaussian_a = gaussian(a, sigma=1, mode='reflect')
     assert gaussian_a.std() < a.std()
 
+
 def test_multichannel():
     pass
+
 
 def test_preserve_range():
     img = np.array([[10.0, -10.0], [-4, 3]], dtype=np.float32)
     gaussian(img, 1, preserve_range=True)
+
 
 def test_gaussian_error_ndim():
     img = np.zeros((5,) * 4)
     with pytest.raises(ValueError):
         gaussian(img, sigma=1)
 
+
 def test_output_type():
-    img = np.arange(16, dtype=np.uint8).reshape((4,4))
+    img = np.arange(16, dtype=np.uint8).reshape((4, 4))
     output_type = np.uint8
     gaussian_img = gaussian(img, 1, output=output_type)
     assert gaussian_img.dtype == output_type
