4d48734d49b28f6888d40573545bf898667ba59c
Nasibli, Samir <samir.nasibli@intel.com>
2019-11-21 03:40:59 -0600

    added test_laplace_skimage_similarity
    * added test_laplace_skimage_similarity, that testing scikit-image's and
      scikit-ipp's laplace filtering results

--- skipp/filters/tests/test_laplace.py
+++ skipp/filters/tests/test_laplace.py
@@ -6,6 +6,16 @@ from skipp.skipp.filters import laplace as skipp_laplace
 from skimage.filters import laplace as skimage_laplace
 
 
+@pytest.fixture
+def image():
+    return np.array([[1, 2, 3, 2, 1],
+                     [1, 1, 2, 2, 3],
+                     [3, 2, 1, 2, 1],
+                     [3, 2, 1, 1, 1],
+                     [1, 2, 1, 2, 3]],
+                    dtype=np.uint8)
+
+
 def test_laplace_zeros():
     """Laplace on a square image."""
     # Create a synthetic 2D image
@@ -22,3 +32,14 @@ def test_laplace_zeros():
                              [0., 0., 0., 0., 0., 0., 0., 0., 0.],
                              [0., 0., 0., 0., 0., 0., 0., 0., 0.]])
     assert_allclose(result, check_result)
+
+
+def test_laplace_skimage_similarity(image):
+    """
+    # Testing scikit-image's and scikit-ipp's laplace filtering results
+    # for float32 input/output dtypes
+    """
+    image = image.astype(np.float32)
+    skimage_laplace_result = skimage_laplace(image)
+    skipp_laplace_result = skipp_laplace(image)
+    assert_array_almost_equal(skimage_laplace_result, skipp_laplace_result)
