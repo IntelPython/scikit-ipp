38b2e4e38d43f5e7795a821e7cd964bf1d4dae1c
Nasibli, Samir <samir.nasibli@intel.com>
2019-11-27 08:54:29 -0600

    added tests for filters
    * unskipped test_sobel_horizontal for sobel filter
    * added test_sobel_h_horizontal, test_sobel_h_vertical for sobel_h filter
    * added test_sobel_v_horizontal, test_sobel_v_vertical for sobel_v filter
    * added test_prewitt_mask, test_prewitt_horizontal, test_prewitt_vertical for prewitt filter
    * added test_prewitt_v_mask, test_prewitt_v_vertical, test_prewitt_v_horizontal for prewitt_v filter
    * added test_prewitt_h_mask, test_prewitt_h_vertical, test_prewitt_h_horizontal for prewitt_v filter

--- skipp/filters/tests/test_sobel.py
+++ skipp/filters/tests/test_sobel.py
@@ -25,7 +25,6 @@ def test_sobel_mask():
     assert(np.all(result == 0))
 
 
-@pytest.mark.skip(reason="deveoloping in progress")
 def test_sobel_horizontal():
     """Sobel on a horizontal edge should be a horizontal line."""
     i, j = np.mgrid[-5:6, -5:6]
@@ -54,6 +53,33 @@ def test_sobel_h_zeros():
     assert(np.all(result == 0))
 
 
+@pytest.mark.skip(reason="deveoloping _mask_filter_result in progress")
+def test_sobel_h_mask():
+    """Horizontal Sobel on a masked array should be zero."""
+    result = skipp_sobel_h(np.random.uniform(size=(10, 10)),
+                           np.zeros((10, 10), dtype=bool))
+    assert_allclose(result, 0)
+
+
+def test_sobel_h_horizontal():
+    """Horizontal Sobel on an edge should be a horizontal line."""
+    i, j = np.mgrid[-5:6, -5:6]
+    image = (i >= 0).astype(np.float32)
+    result = skipp_sobel_h(image)
+    # Check if result match transform direction
+    i[np.abs(j) == 5] = 10000
+    assert (np.all(result[i == 0] == 1))
+    assert (np.all(result[np.abs(i) > 1] == 0))
+
+
+def test_sobel_h_vertical():
+    """Horizontal Sobel on a vertical edge should be zero."""
+    i, j = np.mgrid[-5:6, -5:6]
+    image = (j >= 0).astype(np.float32) * np.sqrt(2)
+    result = skipp_sobel_h(image)
+    assert_allclose(result, 0, atol=1e-10)
+
+
 def test_sobel_v_zeros():
     """Vertical sobel on an array of all zeros."""
     result = skipp_sobel_v(np.zeros((10, 10), dtype=np.float32))
@@ -68,11 +94,22 @@ def test_sobel_v_mask():
     assert_allclose(result, 0)
 
 
-@pytest.mark.skip(reason="deveoloping _mask_filter_result in progress")
-def test_sobel_h_mask():
-    """Horizontal Sobel on a masked array should be zero."""
-    result = skipp_sobel_h(np.random.uniform(size=(10, 10)),
-                           np.zeros((10, 10), dtype=bool))
+def test_sobel_v_vertical():
+    """Vertical Sobel on an edge should be a vertical line."""
+    i, j = np.mgrid[-5:6, -5:6]
+    image = (j >= 0).astype(np.float32)
+    result = skipp_sobel_v(image)
+    # Check if result match transform direction
+    j[np.abs(i) == 5] = 10000
+    assert (np.all(result[j == 0] == 1))
+    assert (np.all(result[np.abs(j) > 1] == 0))
+
+
+def test_sobel_v_horizontal():
+    """vertical Sobel on a horizontal edge should be zero."""
+    i, j = np.mgrid[-5:6, -5:6]
+    image = (i >= 0).astype(np.float32)
+    result = skipp_sobel_v(image)
     assert_allclose(result, 0)
 
 # TODO
