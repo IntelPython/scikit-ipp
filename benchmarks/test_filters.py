81b6f77096193d67ec3820a009ed438547ec24d6
Samir Nasibli <samir.nasibli@intel.com>
2020-02-04 16:20:42 -0600

    update filters benchmark tests
    added changes for filters benchmark test from #PR11

--- benchmarks/test_filters.py
+++ benchmarks/test_filters.py
@@ -1,78 +1,77 @@
 import pytest
 import numpy as np
-from numpy.testing import assert_array_almost_equal
-from scipy.ndimage.filters import gaussian_filter as scipy_gaussian
-from skipp.skipp.filters import gaussian
-from skimage.filters import gaussian as skimage_gaussian
 
+import skimage.filters
+import skipp.skipp.filters
+import scipy.ndimage.filters
 
-def get_image_data(image_dtype):
-    image = np.arange(3000*40000, dtype=image_dtype).reshape((3000, 40000))
+def get_image_data(image_dtype, shape=(300, 400)):
+    image = np.arange(shape[0] * shape[1], dtype=image_dtype).reshape(shape)
     return image
 
 
-def run_scipy_gaussian(image, output, sigma):
-    result = scipy_gaussian(image, output=output, sigma=sigma)
-
-
-def run_skimage_gaussian(image, output, sigma, preserve_range):
-    result = skimage_gaussian(image, output=output, sigma=sigma, preserve_range=preserve_range)
-
-
-def run_skipp_gaussian(image, output, sigma, preserve_range):
-    result = gaussian(image, output=output, sigma=sigma, preserve_range=preserve_range)
-
-@pytest.mark.skip(reason="skipp")
-@pytest.mark.parametrize("input_dtype", [np.uint8, np.int8, np.uint16, np.int16, np.uint32, np.int32, np.float32])
-def test_bench_skimage_gaussian_preserve_range(benchmark, input_dtype):
-    """
-    Speed test, that measures the speed of scikit-image's gaussian filter with given values of parameters:
-    sigma - big sigma (100)
-    output dtype = np.float32 (all input images will be converted into float32)
-    preserve_range is False: signed/unsigned images will be converted into float32 in range [-1..1] or [0..1]
-    """
-    image = get_image_data(input_dtype)
-    # gaussian(image, output=np.float32, sigma=100, preserve_range=False
-    result = benchmark(run_skimage_gaussian, image, np.float32, 100, False)
-
-@pytest.mark.skip(reason="skipp")
-@pytest.mark.parametrize("input_dtype", [np.uint8, np.int8, np.uint16, np.int16, np.uint32, np.int32, np.float32])
-def test_bench_skipp_gaussian_preserve_range(benchmark, input_dtype):
-    """
-    Speed test, that measures the speed of scikit-ipp's gaussian filter with given values of parameters:
-    sigma - big sigma (100)
-    output dtype = np.float32 (all input images will be converted into float32)
-    preserve_range is False: signed/unsigned images will be converted into float32 in range [-1..1] or [0..1]
-    """
-    image = get_image_data(input_dtype)
-    result = benchmark(run_skipp_gaussian, image, np.float32, 100, False)
-
-@pytest.mark.skip(reason="skipp")
 @pytest.mark.parametrize("input_dtype", [np.uint8, np.uint16, np.int16, np.float32])
-def test_bench_skipp_gaussian_with_ipp_supported(benchmark, input_dtype):
+@pytest.mark.parametrize("sigma", [pytest.param(1, id="1.0"),
+                                   pytest.param(100, id="100.0")
+                                   ])
+@pytest.mark.parametrize("shape", [pytest.param((300, 400), id="300x400")
+                                   ])
+@pytest.mark.parametrize("function",[pytest.param(skipp.skipp.filters.gaussian, id="skipp_gaussina"),
+                                     pytest.param(skimage.filters.gaussian, id="skimage_gaussian"),
+                                     pytest.param(scipy.ndimage.filters.gaussian_filter, id="scipy_gaussian")
+                                     ])
+def test_gaussian(benchmark, function, input_dtype, shape, sigma):
     """
-    Speed test, that measures  the speed of scikit-ipp's gaussian filter.
-    scikit-ipp uses Intel IPP ippiFilterGaussianBorder function for the gaussian blur.
-    ippiFilterGaussianBorder supportes only np.uint8, np.uint16, np.int16, np.float32,
-    those no need in converting if the given input & ouput are the same dtype and they are
-    uint8, uint16, int16 or float32.
+    # Speed test, that measures the speed of scikit-ipp's and scikit-image's gaussian filter
     """
-    image = get_image_data(input_dtype)
-    result = benchmark(run_skipp_gaussian, image, None, 100, False)
+    image = get_image_data(input_dtype, shape)
+    result = benchmark(function, image, sigma)
 
-@pytest.mark.skip(reason="skipp")
-@pytest.mark.parametrize("input_dtype", [np.uint8, np.uint16, np.int16, np.float32])
-def test_bench_scipy_gaussian_with_ipp_supported(benchmark, input_dtype):
-    """
-    # Speed test, that measures  the speed of scipy's gaussian filter.
-    #
-    # Note: there is a bug in gaussian scikit-image version 0.17.dev0
-    # skimage.filters.gaussian doesn't use the value of output parameter
-    # skimage.filters.gaussian is a wrapper around scipy.ndi.gaussian_filter. But 
-    # the scikit-image's gaussian doesn't pass the output to scipy.ndi.gaussian_filter.
-    # This leads that skimage.filters.gaussian returns only the image of float64/float32 dtype,
-    # even if explicitly specify the integer output dtype.
-    """
-    image = get_image_data(input_dtype)
-    result = benchmark(run_scipy_gaussian, image, None, 100)
 
+@pytest.mark.parametrize("input_dtype", [np.float32])
+@pytest.mark.parametrize("shape", [pytest.param((300, 400), id="300x400")
+                                   ])
+@pytest.mark.parametrize("function",[pytest.param(skipp.skipp.filters.sobel, id="skipp_sobel"),
+                                     pytest.param(skimage.filters.sobel, id="skimage_sobel"),
+                                     pytest.param(skipp.skipp.filters.sobel_v, id="skipp_sobel_v"),
+                                     pytest.param(skimage.filters.sobel_v, id="skimage_sobel_v"),
+                                     pytest.param(skipp.skipp.filters.sobel_h, id="skipp_sobel_h"),
+                                     pytest.param(skimage.filters.sobel_h, id="skimage_sobel_h"),
+                                     pytest.param(skipp.skipp.filters.prewitt, id="skipp_prewitt"),
+                                     pytest.param(skimage.filters.prewitt, id="skimage_prewitt"),
+                                     pytest.param(skipp.skipp.filters.prewitt_v, id="skipp_prewitt_v"),
+                                     pytest.param(skimage.filters.prewitt_v, id="skimage_prewitt_v"),
+                                     pytest.param(skipp.skipp.filters.prewitt_h, id="skipp_prewitt_h"),
+                                     pytest.param(skimage.filters.prewitt_h, id="skimage_prewitt_h"),
+                                     pytest.param(skipp.skipp.filters.laplace, id="skipp_laplace"),
+                                     pytest.param(skimage.filters.laplace, id="skimage_laplace")
+                                     ])
+def test_edges(benchmark, function, input_dtype, shape):
+    """
+    # Speed test, that measures the speed of scikit-ipp's and scikit-image's filters:
+    # * sobel
+    # * sobel_h
+    # * sobel_v
+    # * prewitt
+    # * prewitt_h
+    # * prewitt_v
+    # * laplace
+    """
+    image = get_image_data(input_dtype, shape)
+    result = benchmark(function, image)
+
+
+@pytest.mark.parametrize("input_dtype", [np.uint8])
+@pytest.mark.parametrize("shape", [pytest.param((300, 400), id="300x400")])
+@pytest.mark.parametrize("mask_shape", [pytest.param((3, 5), id="3x5")])
+@pytest.mark.parametrize("function",[pytest.param(skipp.skipp.filters.median, id="skipp_median"),
+                                     pytest.param(skimage.filters.median, id="skimage_median")])
+def test_median(benchmark, function, input_dtype, shape, mask_shape):
+    """
+    # Speed test, that measures the speed of scikit-ipp's and scikit-image's median filter
+    """
+    image = get_image_data(input_dtype, shape)
+    mask = np.ones(mask_shape, input_dtype)
+    # TODO
+    # set behaviour param to avoid warning
+    result = benchmark(function, image, mask)
