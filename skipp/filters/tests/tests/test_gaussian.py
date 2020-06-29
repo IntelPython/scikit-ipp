3e7fcce94cef2bb2a4197fbbeab9a40c18402168
Nasibli, Samir <samir.nasibli@intel.com>
2020-06-29 18:26:35 +0300

    update gaussian filter test suits (#131)
    update gaussian filter test suits:
    
    * update and enabled `test_gaussian_preserve_dtype` test suit
    * removed outdated test suit `test_gaussian_preserve_output`

--- skipp/filters/tests/tests/test_gaussian.py
+++ skipp/filters/tests/tests/test_gaussian.py
@@ -33,45 +33,18 @@ import numpy as np
 from skipp.filters import gaussian
 
 
-@pytest.fixture
-def image():
-    return np.array([[1, 2, 3, 2, 1],
-                     [1, 1, 2, 2, 3],
-                     [3, 2, 1, 2, 1],
-                     [3, 2, 1, 1, 1],
-                     [1, 2, 1, 2, 3]],
-                    dtype=np.uint8)
-
-
-# TODO
-# add checks for np.uint32, np.int32, np.uint64, np.int64, np.float64, np.double
-# TODO
-# add also checks for different output and input dtypes
-# TODO
-@pytest.mark.skip(reason="update test")
-@pytest.mark.parametrize("input_dtype", [np.uint8, np.int8, np.uint16,
-                                        np.int16, np.float32])
-def test_gaussian_preserve_dtype(image, input_dtype):
-    gaussian_image = gaussian(image.astype(input_dtype))
-    assert gaussian_image.dtype == input_dtype
+@pytest.mark.parametrize("supported_dtype", [np.uint8, np.uint16,
+                                             np.int16, np.float32])
+def test_gaussian_preserve_dtype(supported_dtype):
+    image = np.ones((5, 5), dtype=supported_dtype)
+    filtered_image = gaussian(image)
+    assert filtered_image.dtype == image.dtype
 
 
 def test_gaussian_default_sigma():
     a = np.zeros((3, 3), dtype=np.uint8)
     a[1, 1] = 1.
     assert np.all(gaussian(a) == gaussian(a, sigma=1))
-
-# TODO
-# add checks for np.uint32, np.int32, np.uint64, np.int64, np.float64, np.double
-# currently supported dtypes: np.uint8, np.uint16, np.int16, np.float32
-# TODO
-@pytest.mark.skip(reason="update test")
-@pytest.mark.parametrize("output_dtype", [np.uint8, np.uint16, np.int16, np.float32])
-def test_gaussian_preserve_output(output_dtype):
-    input_image = np.zeros((3, 3), dtype=output_dtype)
-    output_image = np.zeros((3, 3), dtype=output_dtype)
-    returned_image = gaussian(input_image, output=output_image)
-    assert id(output_image) == id(returned_image)
 
 
 def test_gaussian_null_sigma():
