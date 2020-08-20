bf295619bde2081dccb48dd33f1a01bd54b3fc7e
Samir Nasibli <samir.nasibli@intel.com>
2020-08-20 21:53:25 +0300

    Update docs for v1.2.0 (#5)
    * update doc/release_notes.rst
    
    * update doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Intel'
 author = 'Intel'
 
 # The full version, including alpha/beta/rc tags
-release = 'Beta07'
+release = 'v1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
