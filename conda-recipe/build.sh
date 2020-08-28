a9a78e80fefb93da46156caf8984cbc191d6144f
Alexander Rybkin <38352652+xaleryb@users.noreply.github.com>
2020-08-28 22:57:26 +0300

    Update build.sh

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,6 +1,8 @@
 if [ `uname` != Darwin ]; then
 	# currently intel-openmp does not work for osx
     export USE_OPENMP=1
+else
+    export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
 
 LIBROOT=$PREFIX $PYTHON setup.py build install --old-and-unmanageable
