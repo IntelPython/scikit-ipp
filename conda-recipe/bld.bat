8ac79853c150ac30e1254a100170288452be753e
Vyacheslav Smirnov <vyacheslav.s.smirnov@intel.com>
2020-11-16 16:40:19 +0300

    Add wheels build

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -2,5 +2,7 @@
 
 set LIBROOT=%PREFIX%
 set USE_OPENMP=1
-%PYTHON% setup.py build --force install --old-and-unmanageable
+%PYTHON% setup.py build --force install --old-and-unmanageable bdist_wheel
+if errorlevel 1 exit 1
+if NOT "%WHEELS_OUTPUT_FOLDER%"=="" copy dist\scikit-ipp*.whl %WHEELS_OUTPUT_FOLDER%
 if errorlevel 1 exit 1
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+set -ex
+
 if [ `uname` != Darwin ]; then
+    WHEELS_BUILD_ARGS="-p manylinux1_x86_64"
 	# currently intel-openmp does not work for osx
     export USE_OPENMP=1
 else
+    WHEELS_BUILD_ARGS=""
     export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
 
-LIBROOT=$PREFIX $PYTHON setup.py build install --old-and-unmanageable
+export LIBROOT=$PREFIX
+$PYTHON setup.py build install --old-and-unmanageable bdist_wheel ${WHEELS_BUILD_ARGS}
+if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
+    cp dist/scikit-ipp*.whl ${WHEELS_OUTPUT_FOLDER}
+fi
