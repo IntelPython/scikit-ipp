6705e06ba12efd2e4c404ed05aec1aff81cb414b
Vyacheslav-Smirnov <51660067+Vyacheslav-Smirnov@users.noreply.github.com>
2020-11-19 13:13:00 +0300

    Add wheels build (#20)
    * Add wheels build
    
    * Change build format
    
    * Add wheel to host requirements
    
    * Set wheel name for package = scikit-ipp
    
    * Revert package name back to skipp

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+set -ex
+
 if [ `uname` != Darwin ]; then
+    WHEELS_BUILD_ARGS="-p manylinux1_x86_64"
 	# currently intel-openmp does not work for osx
     export USE_OPENMP=1
 else
+    WHEELS_BUILD_ARGS=""
     export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
 
-LIBROOT=$PREFIX $PYTHON setup.py build install --old-and-unmanageable
+export LIBROOT=$PREFIX
+$PYTHON setup.py build install --old-and-unmanageable
+
+# Build wheel package
+if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
+    $PYTHON setup.py bdist_wheel ${WHEELS_BUILD_ARGS}
+    cp dist/skipp*.whl ${WHEELS_OUTPUT_FOLDER}
+fi
