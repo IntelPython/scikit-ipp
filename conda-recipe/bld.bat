bab8f2e8f1baf5f428fb941f2e614561f0f416ee
Vyacheslav-Smirnov <51660067+Vyacheslav-Smirnov@users.noreply.github.com>
2021-03-19 15:38:52 +0300

    Merge changes from gold/2021 to master (#25)
    * License update for beta release
    
    Build number increased, legal notice added
    
    * Indentation fix
    
    * Indentation fix #2
    
    * Indentation fix #3
    
    * Add license_file
    
    * Update meta.yaml
    
    * Update setup.py
    
    * Update meta.yaml
    
    * Revert "Update setup.py"
    
    This reverts commit 1143e0b0f69cff408f49f9c1c60550fe3afed8af.
    
    * Update build.sh
    
    * Add wheels build (#20)
    
    * Add wheels build
    
    * Change build format
    
    * Add wheel to host requirements
    
    * Set wheel name for package = scikit-ipp
    
    * Revert package name back to skipp
    
    * Update linux build tah for wheels (#21)
    
    * Update packages wheel deps (#22)
    
    * Update packages wheel deps
    
    * Update wheel name in build scripts
    
    * Update development status in setup.py (#23)
    
    * Bump build number
    
    To rebuild wheel without strict numpy version
    
    * Add back c compiler for Mac
    
    * Update name to scikit-ipp
    
    Co-authored-by: ekomarova <70146207+ekomarova@users.noreply.github.com>
    Co-authored-by: DmitryKizaev <43135840+DmitryKizaev@users.noreply.github.com>
    Co-authored-by: Alexander Rybkin <38352652+xaleryb@users.noreply.github.com>
    Co-authored-by: Alexander Rybkin <alexander.rybkin@intel.com>

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -4,3 +4,11 @@ set LIBROOT=%PREFIX%
 set USE_OPENMP=1
 %PYTHON% setup.py build --force install --old-and-unmanageable
 if errorlevel 1 exit 1
+
+rem Build wheel package
+if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
+    %PYTHON% setup.py bdist_wheel
+    if errorlevel 1 exit 1
+    copy dist\scikit_ipp*.whl %WHEELS_OUTPUT_FOLDER%
+    if errorlevel 1 exit 1
+)
